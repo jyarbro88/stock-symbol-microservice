0e5fb1845ca8cd1d56bd066cb764ed4986c43e1a
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-22 13:42:06 -0500

    removed commented out code

--- src/main/java/com/microservices/stocks/StocksApplication.java
+++ src/main/java/com/microservices/stocks/StocksApplication.java
@@ -2,15 +2,12 @@ package com.microservices.stocks;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-//@EnableAutoConfiguration(exclude = {
-//        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 public class StocksApplication {
 
     public static void main(String[] args) {
